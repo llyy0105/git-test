10a5db365a2a2263982f7351b2f7cfc143444499
ly <ly@qq.com>
2022-10-21 22:20:05 +0800

    ly first commit

---
+++ src/main/java/com/ly/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ly.git;
+
+/**
+ * @author ly
+ * @create 2022-10-21 22:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
